c83e786fbe7b910674bc8cbed2dfbb0d018716f4
MrTonyJohns <143044602+MrTonyJohns@users.noreply.github.com>
2023-12-05 20:44:42 -0600

    So. Many. Changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
-$("document").ready(function () {
-  $("#accordion").mousedown(function () {
-    $("h3").filter(".ui-accordion").css("background-color", "#f5f5dc");
-  });
-  $("#accordion").mouseup(function () {
-    $("h3").filter(".ui-accordion").css("border", "2px solid #222222");
-  });
-});
-
+//Contact form
 $(function () {
   $("#accordion").accordion();
 });
@@ -57,70 +49,29 @@ $(function () {
   });
 })();
 
-$(function () {
-  $("#dialog").dialog({
-    autoOpen: false,
-    show: {
-      effect: "blind",
-      duration: 1000,
-    },
-    hide: {
-      effect: "blind",
-      duration: 500,
-    },
+//Accordion
+$("document").ready(function () {
+  $("#accordion").mousedown(function () {
+    $("h3").filter(".ui-accordion").css("background-color", "#f5f5dc");
   });
-
-  $("#open").on("click", function () {
-    $("#dialog").dialog("open");
+  $("#accordion").mouseup(function () {
+    $("h3").filter(".ui-accordion").css("border", "2px solid #222222");
   });
 });
 
-$(document).ready(function ($) {
-  $(".lightbox-trigger").click(function (e) {
-    //prevent default action (hyperlink)
-    e.preventDefault();
-
-    //Get clicked link href
-    let image_href = $(this).attr("href");
-
-    /* 	
-		If the lightbox window HTML already exists in document, 
-		change the img src to to match the href of whatever link was clicked
-		
-		If the lightbox window HTML doesn't exists, create it and insert it.
-		(This will only happen the first time around)
-		*/
-
-    if ($("#lightbox").length > 0) {
-      // #lightbox exists
-
-      //place href as img src value
-      $("#content").html('<img src="' + image_href + '" />');
-
-      //show lightbox window - you could use .show('fast') for a transition
-      $("#lightbox").show();
-    } else {
-      //#lightbox does not exist - create and insert (runs 1st time only)
-
-      //create HTML markup for lightbox window
-      let lightbox =
-        '<div id="lightbox">' +
-        "<p>Close</p>" +
-        '<div id="content">' + //insert clicked link's href into img src
-        '<img src="' +
-        image_href +
-        '" />' +
-        "</div>" +
-        "</div>";
-
-      //insert lightbox HTML into page
-      $("body").append(lightbox);
-    }
+//Resize
+$(function () {
+  $("#resize-animate").resizable({
+    animate: true,
   });
+});
 
-  //Click anywhere on the page to get rid of lightbox window
-  $("body").on("click", "#lightbox", function () {
-    //must use on, as the lightbox element is inserted into the DOM
-    $("#lightbox").hide();
-  });
+//Select
+$(function () {
+  $("#select").selectable();
+});
+
+//Tabs
+$(function () {
+  $("#tabs").tabs();
 });
